e8231a024de97b1627de04f64756f1f3dc143511
hainguyen4c <hainguyen.it@hotmail.com>
2015-07-07 23:06:51 +0700

    sample
    sample

---
+++ Source/BasicSitecore_HN/BasicSitecore_HN/layouts/HaiNguyen/Main.aspx.cs
@@ -0,0 +1,13 @@
+namespace BasicSitecore_HN.layouts
+{
+    using System;
+    using System.Web;
+    using System.Web.UI;
+
+    public partial class Main : Page
+    {
+        private void Page_Load(object sender, System.EventArgs e)
+        {
+        }
+    }
+}
